fa6d690ae2f3ea78fda007e9d83af4bd300b3eae
Geovana <102995126+Geovana2007@users.noreply.github.com>
2024-10-03 11:19:01 -0300

    Update common.js

--- graficos/common.js
+++ graficos/common.js
@@ -1,13 +1,12 @@
- const getCSS = (variavel) => {
-     const bodyStyles = getComputedStyle(document.body)
+const getCSS = (variavel) => {
+    const bodyStyles = getComputedStyle(document.body)
     return bodyStyles.getPropertyValue(variavel)
-  }
-  
-  const tickConfig = {
-    color: getCSS('--primary-color'),
+}
+
+const tickConfig = {
+    family: getCSS('--font'),
     size: 16,
-    family: getCSS('--font')
-  }
-  
-  export { getCSS, tickConfig }
-  
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
